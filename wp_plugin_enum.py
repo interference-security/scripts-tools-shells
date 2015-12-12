4ac1ed1e3332720dd237992f22fc6828b8d9fc40
Interference Security <int3rf3r3nc3@gmail.com>
2015-12-12 15:52:20 +0530

    Added argparse for input

--- wp_plugin_enum.py
+++ wp_plugin_enum.py
@@ -1,14 +1,23 @@
 try:
 	import requests
-	import sys
+	import argparse
 	import ssl
 except Exception,e:
 	print "[!] Error: "+str(e)
-	print "[*] Make sure you have the following Python modules installed:\n\trequests, sys, ssl"
+	print "[*] Make sure you have the following Python modules installed:\n\trequests, argparse, ssl"
 	exit(0)
-if len(sys.argv)!=3:
-	print "Usage: %s <path_to_plugins.txt> <target>"%sys.argv[0]
-	exit(0)
+
+parser = argparse.ArgumentParser(description="WordPress plugin enumeration")
+parser.add_argument('-t','--target', help='WordPress target', required=True)
+parser.add_argument('-p','--plugins', help='File containing plugin names', required=True)
+parser.add_argument('-o','--outfile', help='Save output in file')
+parser.add_argument('-v','--verbose', help='Show verbose message', action='store_const', const=True)
+args = parser.parse_args()
+target = args.target.encode('utf-8')
+plugin_file = args.plugins.encode('utf-8')
+
+if target.endswith("/"):
+	target = target[:-1]
 
 if hasattr(ssl, '_create_unverified_context'):
 	ssl._create_default_https_context = ssl._create_unverified_context
@@ -20,16 +29,24 @@ try:
 except:
     pass
 
-f = open(sys.argv[1],"r")
+f = open(plugin_file, "r")
 data = f.readlines()
+
 print "\nNote: Append readme.txt or changelog.txt in same or different letter cases to open version file\n"
+
 for i in data:
 	try:
 		i = (i.replace("\r","")).replace("\n","")
-		r = requests.get(sys.argv[2]+"/wp-content/plugins/"+i+"/", verify=False)
+		if args.verbose:
+			print "[-] Trying: " + i
+		r = requests.get(target+"/wp-content/plugins/"+i+"/", verify=False)
 		sc = r.status_code
-		if sc != 404 and sc != 500:
-			print i + " : " + str(sc) + " : " + sys.argv[2] + "/wp-content/plugins/" + i + "/"
-	except:
+		if sc != 404 and sc != 500 and sc != 403:
+			print i + " : " + str(sc) + " : " + target + "/wp-content/plugins/" + i + "/"
+			if args.outfile:
+				f = open(args.outfile, "a")
+				f.write(i + " : " + str(sc) + " : " + target + "/wp-content/plugins/" + i + "/" + "\n")
+				f.close()
+	except Exception,e:
 		print "Exception occurred"
 print "[*] Completed"
